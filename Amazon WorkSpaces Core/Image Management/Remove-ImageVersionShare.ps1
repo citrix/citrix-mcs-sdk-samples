c92f7c28abb7f8a84ff34ae72c3243217c1097cd
Demetrios Tsillas <Demetrios.Tsillas@cloud.com>
2025-08-28 15:10:14 -0400

    Add scripts for WSC Image Sharing (cloud126)

---
+++ Amazon WorkSpaces Core/Image Management/Remove-ImageVersionShare.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    Share an image version with the specified Hosting Unit.
+.DESCRIPTION
+    Remove-ImageVersionShare.ps1 removes a sharesd image version from the specified Hosting Unit.
+    The original version of this script is compatible with Citrix DaaS September 2025 Release (DDC 126).
+.INPUTS
+    1. ImageVersionSpecUid: Uid of the given image version.
+    2. HostingUnitName: Name of the hosting unit used.
+    3. RunAsynchronously: optionally run the command as a background task
+#>
+
+# /*************************************************************************
+# * Copyright © 2025. Cloud Software Group, Inc. All Rights Reserved.
+# * This file is subject to the license terms contained
+# * in the license file that is distributed with this file.
+# *************************************************************************/
+
+param(
+    [string]$ImageVersionSpecUid,
+    [string]$HostingUnitName,
+    [boolean]$RunAsynchronously(Mandatory = $false)][boolean]$false
+)
+
+Add-PSSnapin -Name "Citrix.Host.Admin.V2", "Citrix.MachineCreation.Admin.V2"
+
+Remove-ProvImageVersionSpecHostingUnit -ImageVersionSpecUid $ImageVersionSpecUid -HostingUnitName $HostingUnitName
