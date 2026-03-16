a8ec79b76e60601fc50421b3d41cb815955faf0d
Nischal Badarinath Kashyap <nischalbadarinath.kashyap@citrix.com>
2026-03-16 15:56:36 +0530

    PMCS-59379 github cmdlet

---
+++ Azure/MaintenanceCycle/HardwareSchemeUpdate/Get SchemeHistory/Get-SchemeHistory.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+    Gets a Provisioning Scheme History. Applicable for Citrix DaaS and on-prem.
+.DESCRIPTION
+    Get-SchemeHistory gets a Provisioning Scheme History
+#>
+
+# /*************************************************************************
+# * Copyright © 2025. Cloud Software Group, Inc. All Rights Reserved.
+# * This file is subject to the license terms contained
+# * in the license file that is distributed with this file.
+# *************************************************************************/
+
+Add-PSSnapin -Name "Citrix.Host.Admin.V2","Citrix.MachineCreation.Admin.V2"
+
+#####################
+# Prepare Parameters
+#####################
+$provisioningSchemeName = "ScaleTest"
+$ProvisioningSchemeHistoryVersion = 1
+
+###############################################################
+# Gets Provisioning Scheme History
+###############################################################
+Get-ProvSchemeHistory -ProvisioningSchemeName $provisioningSchemeName -ProvisioningSchemeHistoryVersion $ProvisioningSchemeHistoryVersion
